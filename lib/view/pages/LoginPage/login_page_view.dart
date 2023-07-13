7766fc076da77616578dbc396c6c1d0028f61a10
M. Sertaç Toröz <msertactoroz@gmail.com>
2023-07-13 13:59:24 +0300

    Login page UI is done

--- lib/view/pages/LoginPage/login_page_view.dart
+++ lib/view/pages/LoginPage/login_page_view.dart
@@ -1,10 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:lineup/colors/colors.dart';
 
 class LoginPageView extends StatelessWidget {
   const LoginPageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              width: 200,
+              height: 200,
+              child: Image.asset(
+                "assets/images/logo.png",
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                hintText: "example@gmail.com",
+                labelText: "e-mail",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                hintText: "example@gmail.com",
+                labelText: "e-mail",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(15),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height / 12,
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: mainColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text("login"),
+              ),
+            ),
+          ),
+          Divider(
+            indent: 50,
+            endIndent: 50,
+            color: mainColor,
+            thickness: 3,
+          ),
+          TextButton(onPressed: () {}, child: Text('Register'))
+        ],
+      ),
+    );
   }
 }
