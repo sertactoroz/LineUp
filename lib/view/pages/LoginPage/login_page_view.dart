ca465b745a74220df9ae03a27c07d58d9ee70024
M. Sertaç Toröz <msertactoroz@gmail.com>
2023-07-13 14:35:06 +0300

    Register button and password hide option added

--- lib/view/pages/LoginPage/login_page_view.dart
+++ lib/view/pages/LoginPage/login_page_view.dart
@@ -1,79 +1,114 @@
 import 'package:flutter/material.dart';
 import 'package:lineup/colors/colors.dart';
 
-class LoginPageView extends StatelessWidget {
+class LoginPageView extends StatefulWidget {
   const LoginPageView({super.key});
+
+  @override
+  State<LoginPageView> createState() => _LoginPageViewState();
+}
+
+class _LoginPageViewState extends State<LoginPageView> {
+  final formKey = GlobalKey<FormState>();
+
+  TextEditingController emailController = TextEditingController();
+
+  TextEditingController passwordController = TextEditingController();
+
+  bool isHided = false;
+
+  setIsHided() {
+    isHided = !isHided;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: SizedBox(
-              width: 200,
-              height: 200,
-              child: Image.asset(
-                "assets/images/logo.png",
+      body: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: SizedBox(
+                width: 200,
+                height: 200,
+                child: Image.asset(
+                  "assets/images/logo.png",
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: "example@gmail.com",
-                labelText: "e-mail",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(15),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  hintText: "example@gmail.com",
+                  labelText: "e-mail",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: "example@gmail.com",
-                labelText: "e-mail",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(15),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: TextFormField(
+                obscureText: isHided,
+                controller: passwordController,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          setIsHided();
+                        },
+                      );
+                    },
+                    icon: isHided
+                        ? const Icon(Icons.remove_red_eye)
+                        : const Icon(Icons.remove_red_eye_outlined),
+                  ),
+                  hintText: "* * * * * *",
+                  labelText: "password",
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height / 12,
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: mainColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 20, right: 20, top: 10, bottom: 10),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height / 12,
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: mainColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
                   ),
+                  onPressed: () {},
+                  child: const Text("login"),
                 ),
-                onPressed: () {},
-                child: const Text("login"),
               ),
             ),
-          ),
-          Divider(
-            indent: 50,
-            endIndent: 50,
-            color: mainColor,
-            thickness: 3,
-          ),
-          TextButton(onPressed: () {}, child: Text('Register'))
-        ],
+            Divider(
+              indent: 50,
+              endIndent: 50,
+              color: mainColor,
+              thickness: 3,
+            ),
+            TextButton(onPressed: () {}, child: const Text('Register'))
+          ],
+        ),
       ),
     );
   }
