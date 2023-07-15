08c40e47be4f08f005b61a93b4e9a430a9f946c0
M. Sertaç Toröz <msertactoroz@gmail.com>
2023-07-15 12:22:33 +0300

    register button functioning

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lineup/view/pages/Init/app_init.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'LineUp',
